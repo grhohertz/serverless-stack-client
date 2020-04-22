30260e12f2ba268dbdbf1af5b20596ce81f7d339
APKAS3C7Y6G5QQDJULMX <gregory@hohertz.com>
2020-04-21 23:37:59 -0700

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "hohertz-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://wrrn6z495l.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Nv5rx43Lm",
+    APP_CLIENT_ID: "7qkeuvreec0bb8p03kjcepur9p",
+    IDENTITY_POOL_ID: "us-east-1:6e7d7fb0-5bb4-45b9-9438-6c0f88c3a4e7"
+  }
+};
